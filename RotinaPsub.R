513b369994dedee1d65cf2d3b43d2ccff9281e0e
AlcineiAzevedo <65502006+AlcineiAzevedo@users.noreply.github.com>
2020-07-08 19:11:21 -0300

    Add files via upload

---
+++ RotinaPsub.R
@@ -0,0 +1,15 @@
+# apagar memoria do r
+remove(list=ls())
+# indicar a pasta onde esta salvo o arquivo psubDBC.txt
+setwd("D:/Parcela subdividida")
+#Abrir os dados no R
+D=read.table("psubDBC.txt",head=T)
+#instalar o pacote
+#install.packages("ExpDes.pt")
+#ativar o pacote
+library(ExpDes.pt)
+#abrir o manual
+?ExpDes.pt
+#fazer a analise estatistica
+psub2.dbc(D$Irrigacao,D$Cultivares,D$Bloco,D$Prod,
+          fac.names = c("Irrigacao", "cultivares"))
